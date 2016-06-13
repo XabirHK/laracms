df0ba2763c8ed49cfdab51eecb5b51c31c30ada4
Zabir Haider Khan <xabirkhan@gmail.com>
2016-06-13 15:17:47 +0600

    Created user front end

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,19 @@
-@extends('layouts.app')
+@extends('layouts.master')
+
+
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
 
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+        @include('pages.partial.service')
+
+        @include('pages.partial.portfolio')
+
+        @include('pages.partial.about')
+
+        @include('pages.partial.team')
+
+        @include('pages.partial.clients')
+
+        @include('pages.partial.contact')
+
+@stop
